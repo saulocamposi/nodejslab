8eb33817ef185d7d70a26d9cab20259278bc206c
Saulo Camposi <saulocamposi@gmail.com>
2016-12-25 23:30:32 -0200

    Add object encapsulation node

---
+++ app/objects/person.js
@@ -0,0 +1,22 @@
+function person(){
+
+  var name,age,gender;
+
+  var firstStep = function(){console.log("First Step")};
+
+  return { first : firstStep,
+           name : "Author Name",
+           age : 33,
+           gender : "Male"
+         };
+};
+
+a = person();
+b = a.first;
+b();
+c = a.name;
+console.log(c);
+d = a.age;
+console.log(d);
+e = a.gender;
+console.log(e);
