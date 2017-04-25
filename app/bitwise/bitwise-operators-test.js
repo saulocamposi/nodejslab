af3d23592f953a8c5942f79f967244994b54326e
Saulo Camposi <saulocamposi@gmail.com>
2017-04-25 05:04:18 +0100

    Add bitwise test

---
+++ app/bitwise/bitwise-operators-test.js
@@ -0,0 +1,31 @@
+assert = require('chai').assert;
+
+number = 32;
+binary = parseInt(number, 10).toString(2);
+and_number = 1 & number;
+or_number =  1 | number;
+not_number = ~ number;
+xor_number =  1 ^ number;
+shift_left_number = number << 3;
+shift_rigth_number = number >> 3;
+
+console.log(" Number Base is : " + number);
+console.log(" Number Binary is : " + binary);
+console.log(" Number AND is : " + and_number);
+console.log(" Number OR is : " + or_number);
+console.log(" Number NOT is : " + not_number);
+console.log(" Number XOR is : " + xor_number);
+console.log(" Number SHIFT LEFT is : " + shift_left_number);
+console.log(" Number SHIFT RIGTH is : " + shift_rigth_number);
+
+
+describe("Logs above", function () {
+  it("It must be number", function(){
+    assert(32,number);
+  });
+
+  it("It must be binary", function(){    
+    assert("helo", binary);
+  });
+
+});
