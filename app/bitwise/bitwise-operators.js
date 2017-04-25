fb3c19efa50a83d0c7b8f147647a53e7d570815b
Saulo Camposi <saulocamposi@gmail.com>
2017-04-25 05:03:47 +0100

    Refactor logs

--- app/bitwise/bitwise-operators.js
+++ app/bitwise/bitwise-operators.js
@@ -1,8 +1,5 @@
 number = 32;
-console.log(number);
 binary = parseInt(number, 10).toString(2);
-console.log(binary);
-
 and_number = 1 & number;
 or_number =  1 | number;
 not_number = ~ number;
@@ -10,9 +7,11 @@ xor_number =  1 ^ number;
 shift_left_number = number << 3;
 shift_rigth_number = number >> 3;
 
-console.log(and_number);
-console.log(or_number);
-console.log(not_number);
-console.log(xor_number);
-console.log(shift_left_number);
-console.log(shift_rigth_number);
+console.log(" Number Base is : " + number);
+console.log(" Number Binary is : " + binary);
+console.log(" Number AND is : " + and_number);
+console.log(" Number OR is : " + or_number);
+console.log(" Number NOT is : " + not_number);
+console.log(" Number XOR is : " + xor_number);
+console.log(" Number SHIFT LEFT is : " + shift_left_number);
+console.log(" Number SHIFT RIGTH is : " + shift_rigth_number);
