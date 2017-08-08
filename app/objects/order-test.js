7daee2a2602dbf059981d825bf7acd71400d8456
Saulo Camposi <saulocamposi@gmail.com>
2017-08-08 03:55:25 +0100

    Add Order Test

---
+++ app/objects/order-test.js
@@ -0,0 +1,8 @@
+assert = require('chai').assert;
+
+describe("Test functions prototype", function () {
+
+    it("It must first function", function(){
+      assert(order.first === order.first);
+    });
+}
