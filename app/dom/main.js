d6930376ef2308ea02fa5c2893efed1de0461cb2
saulocamposi <saulocamposi@gmail.com>
2017-09-08 22:12:44 +0100

    Update main js

--- app/dom/main.js
+++ app/dom/main.js
@@ -1,12 +1,55 @@
+function setStack(elements){
+
+}
+
+function createElement(element, addElementStack, document, setStack){
+  var elem = document.createElement(element);
+  addElementStack(elem, setStack);
+  return elem;
+}
+
+function addElementStack(element, setStack){
+  var elements = [];
+  elements.push(element);
+  console.log( "Element were added" + element );
+  setStack(elements);
+}
+
+function logElement(stackElements){
+  stackElements.forEach(function(element){
+    console.log(element);
+  });
+}
+
+
+
+
 console.log(document);
+
 
 var body = document.querySelector('body');
 var section = document.querySelector('section');
 var para = document.querySelector('p');
 
-var paragraph = document.createElement('p');
-var article = document.createElement('article');
+//var paragraph = document.createElement('p');
+//var article = document.createElement('article');
+//var button = document.createElement('button');
 
+var paragraph = createElement('p', addElementStack, document, setStack );
+
+
+
+var article = document.createElement('article');
+var button = document.createElement('button');
+
+
+
+button.setAttribute('id', 'eventClick');
+button.innerHTML = 'Click On me';
+
+document.body.appendChild(button);
+
+console.log(button);
 console.log(paragraph);
 console.log(article);
 console.log(body);
