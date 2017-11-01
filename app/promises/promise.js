46b2e66416fcc1f9d725bde53f9eb7c776082efe
saulocamposi <saulocamposi@gmail.com>
2017-11-01 03:24:25 +0000

    Update -- promise sintaX

--- app/promises/promise.js
+++ app/promises/promise.js
@@ -3,7 +3,13 @@ var promise = function(){
   var b = 0;
 
   return a/b;
-
-}.then(function(){console.log("ok")},function(){ console.log("error")});
+}.then(
+  function(){
+    console.log("ok")
+  },
+  function(){
+    console.log("error")
+  }
+);
 
 promise();
